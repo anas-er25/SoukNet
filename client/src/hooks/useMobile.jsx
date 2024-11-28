542bdee1f30710f8044153e74bcfeca12730e8c8
anas-er25 <errakibianasdev@gmail.com>
2024-11-29 00:50:46 +0100

    ajouter la partie client: start home, footer, search

---
+++ client/src/hooks/useMobile.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState }  from "react"
+const useMobile = (breakpoint = 768) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint)
+    const handleResize = () => { 
+        const checkpoint = window.innerWidth < breakpoint
+        setIsMobile(checkpoint)
+    }
+    useEffect(() => {
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)  // cleanup function to remove event listener on unmounting
+    },[])
+    return [isMobile]
+}
+
+export default useMobile;
